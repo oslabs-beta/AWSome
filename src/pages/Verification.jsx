28959c7856e765cfa44e44ade70c43eeaffe0f10
Jose Andrew <Jandrew205@outlook.com>
2025-01-27 14:31:40 -0500

    successful verification of signup and login of user

--- src/pages/Verification.jsx
+++ src/pages/Verification.jsx
@@ -1,10 +1,36 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { CognitoUser } from 'amazon-cognito-identity-js';
+import userPool from '../pools/userPool';
 
-const Verify = () => {
+const Verify = ({ email }) => {
+  const [verificationCode, setVerificationCode] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleVerification = () => {
+    const cognitoUser = new CognitoUser({
+      Username: email,
+      Pool: userPool,
+    });
+
+    cognitoUser.confirmRegistration(verificationCode, true, (err, result) => {
+      err
+        ? setMessage(`Verification failed: ${err.message}`)
+        : setMessage(`Account verified successfully!`);
+    });
+  };
+
   return (
     <div>
-      <form></form>
+      <h3>Verify your account</h3>
+      <p>Enter the verification code sent to your email</p>
+      <input
+        type='text'
+        placeholder='Verification Code: 123456'
+        value={verificationCode}
+        onChange={(e) => setVerificationCode(e.target.value)}
+      ></input>
+      <button onClick={handleVerification}>Verify</button>
+      <p>{message}</p>
     </div>
   );
 };
