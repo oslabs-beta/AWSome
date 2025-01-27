1dad9d44941cebda809be3a40fef189d7ab9cfab
Chrissy-a <christinajabraham@yahoo.com>
2025-01-27 15:28:24 -0500

    done login

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router';
 
 function Layout() {
   return (
-    <><div className='bg-gray-400 border-gray-200'>
+    <><div className='bg-white border-gray-200'>
       <Outlet />
     </div>
     <div className='hidden h-full bg-gray-200'>
