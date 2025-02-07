c769b9ff8394c69c9136e5d2e47515460556d53b
Jose Andrew <Jandrew205@outlook.com>
2025-02-07 11:59:53 -0500

    url fixed successfully for google signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -11,12 +11,12 @@ function Signup() {
   const [isVerified, setIsVerified] = useState(false);
 
   //url data to redirect to when user wishes to sign up with Google
-  const authUrl = `https://${import.meta.env.VITE_COGNITO_USER_POOL_ID.replace(
+  const authUrl = `https://${import.meta.env.VITE_COGNITO_USER_POOL_ID.toLowerCase().replace(
     '_',
     ''
-  ).toLowerCase()}.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=${
+  )}.auth.us-east-1.amazoncognito.com/login?client_id=${
     import.meta.env.VITE_COGNITO_CLIENT_ID
-  }&response_type=token&scope=email+openid+profile&redirect_uri=https://d84l1y8p4kdic.cloudfront.net&identity_provider=Google`.trim();
+  }&redirect_uri=https%3A%2F%2Fd84l1y8p4kdic.cloudfront.net&response_type=code`;
 
   //function to handle the signup process for our users
   const handleSignups = (event) => {
@@ -28,7 +28,7 @@ function Signup() {
       UserPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
       ClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
     };
-    //makes a userPool instance using the data from above 
+    //makes a userPool instance using the data from above
     const userPool = new CognitoUserPool(poolData);
 
     //Becomes 'false' to begin
