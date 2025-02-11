0ed99e82690824e9919c16b540267365b0bdb766
Chrissy-a <christinajabraham@yahoo.com>
2025-02-11 16:51:31 -0500

    sign up w google DONE

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -12,7 +12,7 @@ const authUrl = `https://${import.meta.env.VITE_COGNITO_USER_POOL_ID.toLowerCase
   ''
 )}.auth.us-east-1.amazoncognito.com/login?client_id=${
   import.meta.env.VITE_COGNITO_CLIENT_ID
-}&redirect_uri=http://localhost:5173/&response_type=code`;
+}&redirect_uri=http://localhost:5173&response_type=code`;
 
 function Login() {
   const [email, setEmail] = useState('');
